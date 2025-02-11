94473051d1f6f01b8406057171741a528fe2861e
lucile_utilisateur <lucile.z@hotmail.fr>
2025-02-11 09:40:01 +0100

    renommage de la variable p + commentaire en doc js

--- src/app/friend/list-friends/list-friends.component.ts
+++ src/app/friend/list-friends/list-friends.component.ts
@@ -8,25 +8,57 @@ import { OneFriendComponent } from '../one-friend/one-friend.component';
   styleUrl: './list-friends.component.css'
 })
 
+/**
+ * Composant représentant la liste des amis.
+ */
 export class ListFriendsComponent {
-  reputation :boolean = false;
-  constructor(){
+  /**
+   * Indique si la réputation est activée.
+   * Initialement à false, elle passe à true après 3 secondes.
+   *
+   * @type {boolean}
+   */
+  reputation: boolean = false;
+  /**
+   * Texte dynamique lié à l'input utilisateur.
+   *
+   * @type {string}
+   */
+  renduDynamique: string = "";
+  /**
+   * Message indiquant l'état de l'ami.
+   *
+   * @type {string}
+   */
+  ami: string = 'Aucun Ami';
+  /**
+   * Crée une instance de ListFriendsComponent.
+   * Après un délai de 3 secondes, la propriété 'reputation' est mise à true.
+   */
+  constructor() {
     setTimeout(() => {
       this.reputation = true;
     }, 3000);
   }
-  p : string = "";
-  ami :string = 'Aucun Ami';
-
-  onUpdateFriendsList(eventInput:any):void{
-    console.log(eventInput);
-    console.log(eventInput.target.value);
-    this.p = eventInput.target.value;
+  /**
+   * Met à jour le rendu dynamique avec la valeur saisie par l'utilisateur.
+   *
+   * @param {any} eventInput - L'événement émis lors de la saisie dans l'input.
+   * @returns {void}
+   */
+  onUpdateFriendsList(eventInput: any): void {
+    this.renduDynamique = eventInput.target.value;
   }
-  ajouterAmi(){
+  /**
+   * Met à jour la propriété 'ami' pour indiquer qu'un ami a été ajouté avec succès.
+   *
+   * @returns {void}
+   */
+  ajouterAmi(): void {
     this.ami = 'Ami Ajouter Avec Succès';
   }
 }
+
 
 
 /* Dans le composant List-Friends: 
