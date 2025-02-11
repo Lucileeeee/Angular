5b065c7b29370c63c2d410955953457167c50150
lucile_utilisateur <lucile.z@hotmail.fr>
2025-02-11 14:25:49 +0100

    avec correction de jeff

--- src/app/friend/list-friends/list-friends.component.ts
+++ src/app/friend/list-friends/list-friends.component.ts
@@ -134,3 +134,40 @@ export class FormComponent {
 }
 
  */
+
+
+//PARTIE NG IF CORRECTION DE JEFF 
+
+/* import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { OneFriendNgmodelComponent } from '../one-friend-ngmodel/one-friend-ngmodel.component';
+
+@Component({
+  selector: 'app-list-friends-conditional-rendering',
+  imports: [FormsModule,NgIf,OneFriendNgmodelComponent],
+  templateUrl: './list-friends-conditional-rendering.component.html',
+  styleUrl: './list-friends-conditional-rendering.component.css'
+})
+export class ListFriendsConditionalRenderingComponent {
+  listFriendsCreated:boolean=false;
+  listFriendsAuth:boolean=false;
+  listFriendsDisplay:boolean=false;
+  listFriendsCreationStatus:string="🥶 Aucun ami ..."
+  listFriendsInputText:string=""
+  constructor(){
+    // setTimeout(() => {
+    //   this.listFriendsAuth = true;
+    // }, 10000);
+  }
+
+  onAddingFriends():void{
+    this.listFriendsCreationStatus=`🥳 Votre ami (${this.listFriendsInputText}) a été ajouté !`;
+    this.listFriendsCreated=!this.listFriendsCreated;
+  }
+  onUpdateFriendsList(eventInput:Event):void{
+    console.log(eventInput);
+    console.log((<HTMLInputElement>eventInput.target).value);
+    this.listFriendsInputText = (<HTMLInputElement>eventInput.target).value;
+  }
+} */
